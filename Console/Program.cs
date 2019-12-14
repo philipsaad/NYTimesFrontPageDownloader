afe7b00162445df30eb16c5b48551c5e954c1dbb
Philip Saad <philipsaad@users.noreply.github.com>
2019-12-13 23:02:22 -0800

    Big refactor: - Upgraded from .NET Core 2.1 to 3.1 - Added NYT icon - Added filter to allLowResolutionScans IEnumerable to remove all Sundays before 04/14/1861 because NYT does not make their Sunday Editions available online - Replaced WaitAll() because it was not respecting any of the TCP/IP limits I was setting and it was very rapidly exhausting my TCP/IP socket pool - Replaced Async File IO with static methods newly ported from .NET Core framework to simplify the code - Replaced GetBytesReadable with ByteSize NuGet package - Added ability to publish app a single EXE file that contains the entire .NET Core, making it work without the .NET Core 3.1 runtime being installed - Added ability to trim said .EXE to remove all unused .NET Core assemlies, making it smaller - Added ability to provide AoT compilation to provide a better experience on Windows, making it faster

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,178 +1,114 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
+using ByteSizeLib;
+
 namespace NYTimesFrontPageDownloader
 {
-    class Program
+  internal class Program
+  {
+    internal static readonly HttpClient client = new HttpClient();
+
+    internal static async Task Main()
     {
-        static HttpClient client = new HttpClient(new HttpClientHandler { MaxConnectionsPerServer = 1024 });
+      //Set start date to start downloading low-resolution front page scans to 09/18/1851; the first date available
+      DateTime lowResolutionStartDate = new DateTime(1851, 09, 18);
 
-        static async Task Main(string[] args)
-        {
-            //Set start date to start downloading low-resolution front page scans to 09/18/1851; the first date available
-            var lowResolutionStartDate = new DateTime(1851, 09, 18);
+      //For whatever reason, the NY Times doesn't make any of its Sunday Editions prior to 04/14/1861 available.
+      DateTime lowResolutionSundayStartDate = new DateTime(1861, 04, 14);
 
-            //Set start date to start downloading high-resolution front page scans to 07/12/2012; the first date available
-            var highResolutionStartDate = new DateTime(2012, 07, 06);
+      //Set start date to start downloading high-resolution front page scans to 07/12/2012; the first date available
+      DateTime highResolutionStartDate = new DateTime(2012, 07, 06);
 
-            //Lets end today, feel free to change this to a different day
-            var highResolutionEndDate = DateTime.Today;
+      //Lets end today, feel free to change this to a different day
+      DateTime highResolutionEndDate = DateTime.Today;
 
-            //Use LINQ to create a list of Uris for every day between the start and end dates
-            var allLowResolutionScans = Enumerable
-              .Range(0, int.MaxValue)
-              .Select(index => new DateTime?(lowResolutionStartDate.AddDays(index)))
-              .TakeWhile(date => date <= highResolutionEndDate)
-              .Select(x => new Uri($"http://www.nytimes.com/images/{x.Value.Year:D4}/{x.Value.Month:D2}/{x.Value.Day:D2}/nytfrontpage/scan.jpg"));
+      //Use LINQ to create a list of Low Resolution Uris for every day between the start and end dates, skipping the Sundays before lowResolutionSundayStartDate
+      IEnumerable<Uri> lowResolutionScans = Enumerable
+        .Range(0, int.MaxValue)
+        .Select(index => new DateTime?(lowResolutionStartDate.AddDays(index)))
+        .TakeWhile(date => date <= highResolutionEndDate)
+        .Where(x => (x.Value < lowResolutionSundayStartDate && x.Value.DayOfWeek != DayOfWeek.Sunday) || x.Value > lowResolutionSundayStartDate)
+        .Select(x => new Uri($"http://www.nytimes.com/images/{x.Value.Year:D4}/{x.Value.Month:D2}/{x.Value.Day:D2}/nytfrontpage/scan.jpg"));
 
-            //Use LINQ to create a list of Uris for every day between the start and end dates
-            var allHighResolutionScans = Enumerable
-              .Range(0, int.MaxValue)
-              .Select(index => new DateTime?(highResolutionStartDate.AddDays(index)))
-              .TakeWhile(date => date <= highResolutionEndDate)
-              .Select(x => new Uri($"http://www.nytimes.com/images/{x.Value.Year:D4}/{x.Value.Month:D2}/{x.Value.Day:D2}/nytfrontpage/scan.pdf"));
+      //Use LINQ to create a list of High Resolution Uris for every day between the start and end dates
+      IEnumerable<Uri> highResolutionScans = Enumerable
+        .Range(0, int.MaxValue)
+        .Select(index => new DateTime?(highResolutionStartDate.AddDays(index)))
+        .TakeWhile(date => date <= highResolutionEndDate)
+        .Select(x => new Uri($"http://www.nytimes.com/images/{x.Value.Year:D4}/{x.Value.Month:D2}/{x.Value.Day:D2}/nytfrontpage/scan.pdf"));
 
-            //Combine the two lists so we only loop through one
-            var allUris = allLowResolutionScans.Concat(allHighResolutionScans);
+      //Combine the two lists so we only loop through one
+      IEnumerable<Uri> allUris = lowResolutionScans.Concat(highResolutionScans);
 
-            client.DefaultRequestHeaders.ExpectContinue = false;
-
-            //Loop through each link and download the front page
-            await Task.WhenAll(allUris.Select(singleLink => nyTimesHelper(singleLink)));
-        }
-
-        //Simple helper method to parse out the dates from the Uri so we can save them in a file naming convention that makes sense
-        static async Task nyTimesHelper(Uri singleLink)
-        {
-            //Example low resolution jpg: http://www.nytimes.com/images/1851/09/18/nytfrontpage/scan.jpg
-            //Example high resolution pdf: http://www.nytimes.com/images/2017/10/02/nytfrontpage/scan.pdf
-            var year = singleLink.Segments[2].Replace("/", "");
-            var month = singleLink.Segments[3].Replace("/", "");
-            var day = singleLink.Segments[4].Replace("/", "");
-            var extension = singleLink.Segments[6].Split('.').Last();
-
-            var fileSavePath = $@"{year}\{month}\{year}_{month}_{day}.{extension}";
-
-            if (File.Exists(fileSavePath))
-            {
-                Console.WriteLine($"File {fileSavePath} already exists; skipped");
-            }
-            else
-            {
-                //Download file in year\month\year_month_day.ext format
-                await downloadFileAsync(singleLink, fileSavePath);
-            }
-        }
-
-        //Download the file asynchronously and write the result to the console
-        static async Task downloadFileAsync(Uri singleLink, string fileSavePath)
-        {
-            //Use HttpRequest instead of WebClient for concurrency
-            var request = new HttpRequestMessage(HttpMethod.Get, singleLink);
-
-            //Make the call, but only read the headers
-            var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
-            //Make sure we have a valid response
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                //Otherwise, lets proceed with the download
-                var httpStream = await response.Content.ReadAsStreamAsync();
-
-                //Check to see if the directory in the file path exists
-                if (!Directory.Exists(fileSavePath))
-                {
-                    //If not, create it
-                    Directory.CreateDirectory(new FileInfo(fileSavePath).DirectoryName);
-                }
-
-                //Create the file on the file system and prep it for saving
-                using (var fileStream = File.Create(fileSavePath, 16384, FileOptions.Asynchronous))
-                {
-                    using (var reader = new StreamReader(httpStream))
-                    {
-                        {
-                            //Write the contents of the filesteam to the file
-                            await httpStream.CopyToAsync(fileStream);
-                            await fileStream.FlushAsync();
-                        }
-                    }
-                }
-
-                //Basic validation
-                if ((new FileInfo(fileSavePath).Exists) && new FileInfo(fileSavePath).Length > 0)
-                {
-                    Console.WriteLine($"File {fileSavePath} downloaded successfully, {GetBytesReadable(new FileInfo(fileSavePath).Length)} saved");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"Error validating file {fileSavePath}");
-                    Console.ResetColor();
-                }
-            }
-            //If we run into some other HTTP status code, lets write that to the console and move on
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"Error downloading file {fileSavePath}: {(int)response.StatusCode} - {response.StatusCode}");
-                Console.ResetColor();
-            }
-        }
-
-        // Returns the human-readable file size for an arbitrary, 64-bit file size 
-        // The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"
-        // Copied from https://stackoverflow.com/a/11124118
-        static string GetBytesReadable(long i)
-        {
-            // Get absolute value
-            long absolute_i = (i < 0 ? -i : i);
-            // Determine the suffix and readable value
-            string suffix;
-            double readable;
-            if (absolute_i >= 0x1000000000000000) // Exabyte
-            {
-                suffix = "EB";
-                readable = (i >> 50);
-            }
-            else if (absolute_i >= 0x4000000000000) // Petabyte
-            {
-                suffix = "PB";
-                readable = (i >> 40);
-            }
-            else if (absolute_i >= 0x10000000000) // Terabyte
-            {
-                suffix = "TB";
-                readable = (i >> 30);
-            }
-            else if (absolute_i >= 0x40000000) // Gigabyte
-            {
-                suffix = "GB";
-                readable = (i >> 20);
-            }
-            else if (absolute_i >= 0x100000) // Megabyte
-            {
-                suffix = "MB";
-                readable = (i >> 10);
-            }
-            else if (absolute_i >= 0x400) // Kilobyte
-            {
-                suffix = "KB";
-                readable = i;
-            }
-            else
-            {
-                return i.ToString("0 B"); // Byte
-            }
-            // Divide by 1024 to get fractional value
-            readable = (readable / 1024);
-
-            // Return formatted number with suffix
-            return readable.ToString("0.## ") + suffix;
-        }
+      //Loop through each link and download the front page. Got rid of the WaitAll() because it was not respecting any of the TCP/IP limits I was setting and exhausting out socket pool
+      foreach (Uri singleUri in allUris)
+      {
+        await DownloadFileAsync(singleUri);
+      }
     }
+
+    //Simple helper method to parse out the dates from the Uri so we can save them in a file naming convention that makes sense
+    internal static async Task DownloadFileAsync(Uri singleLink)
+    {
+      //Example low resolution jpg: http://www.nytimes.com/images/1851/09/18/nytfrontpage/scan.jpg
+      //Example high resolution pdf: http://www.nytimes.com/images/2017/10/02/nytfrontpage/scan.pdf
+      int year = Convert.ToInt32(singleLink.Segments[2].Replace("/", ""));
+      int month = Convert.ToInt32(singleLink.Segments[3].Replace("/", ""));
+      int day = Convert.ToInt32(singleLink.Segments[4].Replace("/", ""));
+      DateTime date = new DateTime(year, month, day);
+
+      //Grab file extension from URI using built-in .NET classes
+      string extension = Path.GetExtension(singleLink.ToString());
+
+      string fileSavePath = $@"{date.Year}\{date.Month}\{date.Year}_{date.Month}_{date.Day}{extension}";
+
+      //Make the call, but only read the headers
+      using (HttpResponseMessage response = await client.GetAsync(singleLink))
+      {
+        //Make sure we have a valid response
+        if (response.IsSuccessStatusCode)
+        {
+          //Otherwise, lets proceed with the download
+          byte[] responseBytes = await response.Content.ReadAsByteArrayAsync();
+
+          //Create a FileInfo with our future file, this simplifies several logic checks we will be doing below
+          FileInfo newFileInfo = new FileInfo(fileSavePath);
+
+          //DirectoryInfo.Create() internally checks if the directory already exists before creating it and will silently shorcut the call.
+          newFileInfo.Directory.Create();
+
+          //Create the file on the file system and write the contents of the byte array to the file
+          await File.WriteAllBytesAsync(fileSavePath, responseBytes);
+
+          //Basic validation
+          if (newFileInfo.Exists && newFileInfo.Length > 0)
+          {
+            Console.WriteLine($"File {fileSavePath} downloaded successfully, {ByteSize.FromBytes(newFileInfo.Length)} saved");
+          }
+          else
+          {
+            WriteError($"Error validating file {fileSavePath}");
+          }
+        }
+        //If we run into some other HTTP status code, lets write that to the console and move on
+        else
+        {
+          WriteError($"Error downloading file {fileSavePath}: {(int)response.StatusCode} - {response.StatusCode}");
+        }
+      }
+    }
+
+
+    internal static void WriteError(string message)
+    {
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine(message);
+      Console.ResetColor();
+    }
+  }
 }
